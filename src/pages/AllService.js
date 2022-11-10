72ca9557f40d944174439e11a5b90e8fd1eab2ea
Sadat Saim <saimk861@gmail.com>
2022-11-10 23:00:25 +0600

    responsive issue fixed

--- src/pages/AllService.js
+++ src/pages/AllService.js
@@ -30,7 +30,7 @@ const AllService = () => {
       <h2 className="text-2xl text-center font-bold mt-10 uppercase border-b-2 pb-3">
         All Services
       </h2>
-      <div className="grid grid-cols-3 gap-3 px-3">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 px-3">
         {services?.map((service) => (
           <ServiceCard key={service._id} service={service}></ServiceCard>
         ))}
